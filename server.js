62d8e939c188779f0e72e0d3102fd197a8a307a5
Alberto Tessarotto <alberto.tessarotto@mia-platform.eu>
2025-09-20 10:53:11 +0200

    Fix: CORS headers espliciti per risolvere blocco GitHub Pages

--- server.js
+++ server.js
@@ -7,6 +7,19 @@ const { calculateDamage, generateEffectivenessDescription } = require('./typeEff
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Middleware CORS esplicito
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+});
+
 // Middleware
 app.use(cors({
   origin: '*',
